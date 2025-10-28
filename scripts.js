8a769d16ae71536359b65e7ff826d518cdab2ce6
smvyas <thevyas19@gmail.com>
2025-10-28 00:00:02 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// JavaScript for toggling the navigation menu
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
